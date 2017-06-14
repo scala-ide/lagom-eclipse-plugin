2eabe71379f44c78ed6d07ca37cdda4de1f56c08
wpopielarski <wpopielarski@gmail.com>
2017-06-14 12:42:21 +0200

    Service Locator and Application interaction.

--- org.scala-ide.sdt.lagom.runner/src/org/scalaide/lagom/launching/LagomLauncher.scala
+++ org.scala-ide.sdt.lagom.runner/src/org/scalaide/lagom/launching/LagomLauncher.scala
@@ -21,6 +21,7 @@ import org.apache.cassandra.io.util.FileUtils
 import com.lightbend.lagom.scaladsl.persistence.cassandra.testkit.TestUtil
 import java.time.LocalDateTime
 import com.lightbend.lagom.discovery.ServiceLocatorServer
+import scala.concurrent.Future
 
 object LagomLauncher {
   val LagomLauncherClass = "org.scalaide.lagom.launching.LagomLauncher$"
@@ -53,44 +54,54 @@ object LagomLauncher {
           val a = classSymbol.toType
           toInvoke().asInstanceOf[LagomApplicationLoader]
         }.map { appLoader =>
-          val config = Configuration.from(Map("lagom.service-locator" -> Map("url" -> "http://127.0.0.1:3467")))
+          val config = Configuration.from(Map("lagom.service-locator" -> Map("url" -> "http://localhost:3467"),
+              ("akka" -> Map(
+                "remote" -> Map(
+                  "netty.tcp" ->
+                    Map("port" -> 2554)
+                  )
+                )
+              )))
 
-          val cassConfig = {
-            val now = DateTimeFormatter.ofPattern("yyMMddHHmmssSSS").format(LocalDateTime.now())
-            val testName = s"ServiceTest_$now"
-            val cassandraPort = CassandraLauncher.randomPort
-            val cassandraDirectory = Files.createTempDirectory(testName).toFile
-            FileUtils.deleteRecursiveOnExit(cassandraDirectory)
-            CassandraLauncher.start(cassandraDirectory, "lagom-test-embedded-cassandra.yaml", clean = false, port = 0)
-            Configuration(TestUtil.persistenceConfig(testName, cassandraPort, useServiceLocator = false)) ++
-              Configuration("lagom.cluster.join-self" -> "on")
-          }
-
+          try {
+          import scala.concurrent.ExecutionContext.Implicits._
+          Future {
           import scala.collection.JavaConverters._
           val serLoc = new ServiceLocatorServer()
           serLoc.start(3467, 0, Map.empty.asJava)
+          }.map {_ =>
 
-          val context = LagomApplicationContext(Context(Environment.simple(mode = Mode.Dev), None, new DefaultWebCommands, config ++ cassConfig))
-          appLoader.logger.error("################ starting... ###############")
-          try {
+//          val cassConfig = {
+//            val now = DateTimeFormatter.ofPattern("yyMMddHHmmssSSS").format(LocalDateTime.now())
+//            val testName = s"ServiceTest_$now"
+//            val cassandraPort = CassandraLauncher.randomPort
+//            val cassandraDirectory = Files.createTempDirectory(testName).toFile
+//            FileUtils.deleteRecursiveOnExit(cassandraDirectory)
+//            CassandraLauncher.start(cassandraDirectory, "lagom-test-embedded-cassandra.yaml", clean = false, port = 0)
+//            Configuration(TestUtil.persistenceConfig(testName, cassandraPort))
+//          }
+
+          val context = LagomApplicationContext(Context(Environment.simple(mode = Mode.Dev), None, new DefaultWebCommands, config /*++ cassConfig*/))
+          appLoader.logger.info("################ starting... ###############")
           val lagomApplication = appLoader.loadDevMode(context)
-          appLoader.logger.error("!!!!!!!!" + lagomApplication.serviceInfo.serviceName)
-          Play.start(lagomApplication.application)
-          val serverConfig = ServerConfig(port = Some(9099), mode = lagomApplication.environment.mode)
+          appLoader.logger.info("!!!!!!!!" + lagomApplication.serviceInfo.serviceName)
+          //Play.start(lagomApplication.application)
+          val serverConfig = ServerConfig(port = Some(9099), mode = lagomApplication.environment.mode, address = "localhost")
           val playServer = ServerProvider.defaultServerProvider.createServer(serverConfig, lagomApplication.application)
-          lagomApplication match {
-            case requiresPort: RequiresLagomServicePort =>
-              requiresPort.provideLagomServicePort(playServer.httpPort.orElse(playServer.httpsPort).get)
-            case other => ()
           }
-          Runtime.getRuntime.addShutdownHook {
-            new Thread { () =>
-              Try(Play.stop(lagomApplication.application))
-              Try(playServer.stop())
-              Try(CassandraLauncher.stop())
-              Try(serLoc.close())
-            }
-          }
+//          lagomApplication match {
+//            case requiresPort: RequiresLagomServicePort =>
+//              requiresPort.provideLagomServicePort(playServer.httpPort.orElse(playServer.httpsPort).get)
+//            case other => ()
+//          }
+//          Runtime.getRuntime.addShutdownHook {
+//            new Thread { () =>
+//              Try(Play.stop(lagomApplication.application))
+//              Try(playServer.stop())
+//              Try(CassandraLauncher.stop())
+//              Try(serLoc.close())
+//            }
+//          }
           (1 to 10).foreach { _ => Thread.sleep(1000) }
           } catch {
             case e: Error => appLoader.logger.error("!!!!!!!!!!!" + e)
