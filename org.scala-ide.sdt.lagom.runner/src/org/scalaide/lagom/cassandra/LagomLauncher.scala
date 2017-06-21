852f813139950270ca4661f384935dba2c25da84
wpopielarski <wpopielarski@gmail.com>
2017-06-21 15:51:27 +0200

    Adds Cassandra runner.

---
+++ org.scala-ide.sdt.lagom.runner/src/org/scalaide/lagom/cassandra/LagomLauncher.scala
@@ -0,0 +1,49 @@
+package org.scalaide.lagom.cassandra
+
+import scala.concurrent.duration.Deadline
+import scala.concurrent.duration.FiniteDuration
+
+import com.datastax.driver.core.Cluster
+import com.lightbend.lagom.internal.cassandra.CassandraLauncher
+
+object LagomLauncher extends App {
+  val port :: _ = args.toList
+  val hostname = "127.0.0.1"
+  private def waitForRunningCassandra(maxWaiting: FiniteDuration): Unit = {
+    import scala.collection.JavaConverters._
+    val contactPoint = Seq(new java.net.InetSocketAddress(hostname, port.toInt)).asJava
+    val clusterBuilder = Cluster.builder.addContactPointsWithPorts(contactPoint)
+
+    @annotation.tailrec
+    def tryConnect(deadline: Deadline): Unit = {
+      print(".") // each attempts prints a dot (informing the user of progress) 
+      try {
+        val session = clusterBuilder.build().connect()
+        println() // we don't want to print the message on the same line of the dots... 
+        session.closeAsync()
+        session.getCluster.closeAsync()
+        println("cassandra started")
+      } catch {
+        case _: Exception =>
+          if (deadline.hasTimeLeft()) {
+            // wait a bit before trying again
+            Thread.sleep(500)
+            tryConnect(deadline)
+          } else {
+            val msg = s"""Cassandra server is not yet started.\n
+                           |The value assigned to
+                           |`lagomCassandraMaxBootWaitingTime`
+                           |is either too short, or this may indicate another 
+                           |process is already running on port ${port}""".stripMargin
+            println(msg) // we don't want to print the message on the same line of the dots...
+          }
+      }
+    }
+    tryConnect(maxWaiting.fromNow)
+  }
+  CassandraLauncher.main(args)
+  new Thread { _ =>
+    import scala.concurrent.duration._
+    waitForRunningCassandra(10 seconds)
+  }.start
+}
